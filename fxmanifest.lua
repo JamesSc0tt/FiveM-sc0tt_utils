331765006b42638f857519055db2ee94f42ff280
JamesSc0tt <jamesscott.js95@gmail.com>
2020-04-18 00:40:10 +0100

    :INIT:

---
+++ fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'bodacious'
+games { 'gta5' }
+
+author 'jamessc0tt'
+description 'Original code by jamessc0tt/KNG - Utils for various sc0tt scripts'
+version '0.0.0'
+
+client_scripts { 
+	'cl_utils.lua',
+}
+
+server_scripts { 
+	'sv_utils.lua',
+}
